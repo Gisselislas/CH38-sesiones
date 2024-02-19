f5e7507959d6b37a05454357b7f159626f455ee4
Gissel Islas <gissislas0493@gmail.com>
2024-02-19 13:25:09 -0600

    ci:nombre persona

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log  ("HOLA CH38");
+function enviarSaludo(){
+    alert('¡Hola Ch38!');
+}
+
+    function enviarSaludoAPersona(){ 
+    let message = prompt ("Cual es tu nombre");
+    alert('Hola ${message}');
+ 
+
+    }
+
